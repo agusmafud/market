292ef7b15e252e776f4a748dc59ed3fad829e8a9
agusmafud <agusmafud@gmail.com>
2019-03-08 13:41:32 -0300

    Versión final

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyB7dFBvSS_sPIXlux4f8yaemvryFEtcr6g",
-    authDomain: "reactjs-market.firebaseapp.com",
-    databaseURL: "https://reactjs-market.firebaseio.com",
-};
