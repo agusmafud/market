0bea11826ec9031351aefd31e92ab8dd3ea8c7bc
agusmafud <agusmafud@gmail.com>
2019-03-08 12:00:52 -0300

    Updates

---
+++ precache-manifest.d24676a7ac6cc12f6caf84b02bde014c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c12631630179cf22c5ae5317cae7370d",
+    "url": "/market/static/media/7.c1263163.jpg"
+  },
+  {
+    "revision": "4721ff54da7ae58d5f36",
+    "url": "/market/static/js/main.4721ff54.chunk.js"
+  },
+  {
+    "revision": "c6846700687ac6840ce9",
+    "url": "/market/static/js/runtime~main.c6846700.js"
+  },
+  {
+    "revision": "a4b1e7c2554c61c61208e4471e7c64d1",
+    "url": "/market/static/media/logo.a4b1e7c2.svg"
+  },
+  {
+    "revision": "f7a32b96bae14b58a5248a39b00caee1",
+    "url": "/market/static/media/1.f7a32b96.png"
+  },
+  {
+    "revision": "81de4331376bd466a2a2ffb009a89434",
+    "url": "/market/static/media/2.81de4331.jpg"
+  },
+  {
+    "revision": "a16db1ddde7309dcb336844eae8dc531",
+    "url": "/market/static/media/3.a16db1dd.png"
+  },
+  {
+    "revision": "271764f38a627b36fc068db54a372029",
+    "url": "/market/static/media/4.271764f3.jpg"
+  },
+  {
+    "revision": "2cbcf7dc5a1660df37bc01679b33cfac",
+    "url": "/market/static/media/5.2cbcf7dc.jpg"
+  },
+  {
+    "revision": "35997f7ccc9a601ee7a47cd88a5b1c68",
+    "url": "/market/static/media/6.35997f7c.jpg"
+  },
+  {
+    "revision": "f54634f0314efa6034446bde58858406",
+    "url": "/market/static/media/3.f54634f0.jpeg"
+  },
+  {
+    "revision": "784e868b89187bd079af87063f89c382",
+    "url": "/market/static/media/8.784e868b.jpg"
+  },
+  {
+    "revision": "9186a106aa2d50dfd4efc94f5ad1a8bc",
+    "url": "/market/static/media/9.9186a106.jpg"
+  },
+  {
+    "revision": "d366d2ca04e780418d8468214472c380",
+    "url": "/market/static/media/10.d366d2ca.jpg"
+  },
+  {
+    "revision": "300ca23c222d505554e86eae1914dca4",
+    "url": "/market/static/media/hero.300ca23c.jpg"
+  },
+  {
+    "revision": "de443ebc35246970d1d8f65f14091b02",
+    "url": "/market/static/media/facebook.de443ebc.svg"
+  },
+  {
+    "revision": "5b79803ad7bd3457a92c292e40ac991b",
+    "url": "/market/static/media/instagram.5b79803a.svg"
+  },
+  {
+    "revision": "094769dec8f54ce50ee9fc239195c5a0",
+    "url": "/market/static/media/1.094769de.jpeg"
+  },
+  {
+    "revision": "a1756cb033fdeef05069c835f77aaf29",
+    "url": "/market/static/media/2.a1756cb0.jpeg"
+  },
+  {
+    "revision": "2d7d166352fb353680ca",
+    "url": "/market/static/js/1.2d7d1663.chunk.js"
+  },
+  {
+    "revision": "4ad3a5ecd97e1d6b7f992374807cd2de",
+    "url": "/market/index.html"
+  }
+];
