8928e7ac9b118d20f7c9a9035fd9797cad290c26
agusmafud <agusmafud@gmail.com>
2019-03-09 15:08:47 -0300

    Versión final

---
+++ src/config/prod.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+    apiKey: "Colocá aquí tu Firebase API Key",
+    authDomain: "Colocá aquí tu Firebase authDomain",
+    databaseURL: "Colocá aquí tu Firebase databaseURL",
+};
